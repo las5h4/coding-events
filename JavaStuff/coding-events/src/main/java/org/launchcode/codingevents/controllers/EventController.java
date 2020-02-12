51875be70d41b4ee2d0ac11046328b3a99477e32
las5h4 <loganasmalley@gmail.com>
2020-02-12 17:31:24 -0600

    Went back and dit data layer video, also completed the exercise

--- JavaStuff/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ JavaStuff/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +13,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -27,7 +27,23 @@ public class EventController {
 
     @PostMapping("create")
     public String createEvent(@ModelAttribute Event newEvent) {
-        events.add(newEvent);
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        String title;
+        title = "Edit Event "+(EventData.getById(eventId)).getName()+" (id = "+eventId+")";
+        model.addAttribute(EventData.getById(eventId));
+        model.addAttribute(title);
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        EventData.getById(eventId).setName(name);
+        EventData.getById(eventId).setDescription(description);
         return "redirect:";
     }
 }
