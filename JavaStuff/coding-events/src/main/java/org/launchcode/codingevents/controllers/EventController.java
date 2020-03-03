b574b73d3920c4ed11ac4d165396d702e97e9f71
las5h4 <loganasmalley@gmail.com>
2020-03-02 19:21:29 -0600

    Did not finish persistent model video, moving on to studio for now

--- JavaStuff/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ JavaStuff/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,9 +19,12 @@ import java.util.List;
 public class EventController {
 
 
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -37,7 +42,7 @@ public class EventController {
             model.addAttribute("errorMsg", "Bad Data!");
             return "events/create";
         } else {
-            EventData.add(newEvent);
+            eventRepository.save(newEvent);
             return "redirect:";
         }
     }
@@ -45,9 +50,10 @@ public class EventController {
     @GetMapping("edit/{eventId}")
     public String displayEditForm(Model model, @PathVariable int eventId) {
         String title;
-        title = "Edit Event "+(EventData.getById(eventId)).getName()+" (id = "+eventId+")";
-        model.addAttribute(EventData.getById(eventId));
+        title = "Edit Event "+(eventRepository.findById(eventId)).toString()+" (id = "+eventId+")";
+        model.addAttribute(eventRepository.findById(eventId));
         model.addAttribute(title);
+
         return "events/edit";
     }
 
